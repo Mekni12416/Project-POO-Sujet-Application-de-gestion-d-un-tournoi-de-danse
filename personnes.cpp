e89b3e1739960d69c3f74be9021e7cbb898d51c3
Mekni Jihed <160278155+Mekni12416@users.noreply.github.com>
2024-02-24 16:02:31 +0100

    add personnes.cpp

--- personnes.cpp
+++ personnes.cpp
@@ -13,7 +13,7 @@ personnes::personnes()
     date_naissance = "";
 }
 // constructeur personnes pour saisir les attributs
-personnes::personnes(int cin, string nom, string prenom, string nationalite, string date_naissance)
+personnes::personnes(int cin_p, string nom, string prenom, string nationalite, string date_naissance)
 {
     this->cin_p = cin_p;
     this->nom = nom;
@@ -35,12 +35,60 @@ void personnes::set_personne()
     cout << "donner date naissance du personne" << endl;
     cin >> date_naissance;
 }
-//methode geter personne
-void personnes::get_personne(){
-    cout<<"******Affichage du personne******"<<endl;
-    cout<<"le cin du personne est :  "<<cin_p<<endl;;
-    cout<<"le nom du personne est :  "<<nom<<endl;
-    cout<<"le prenom du personne est :  "<<prenom<<endl;
-    cout<<"la nationalite du personne est :  "<<nationalite<<endl;
-    cout<<"la date de naissance du personne est :  "<<date_naissance<<endl;
+// methode geter personne
+void personnes::get_personne()
+{
+    cout << "******Affichage du personne******" << endl;
+    cout << "le cin du personne est :  " << cin_p << endl;
+    ;
+    cout << "le nom du personne est :  " << nom << endl;
+    cout << "le prenom du personne est :  " << prenom << endl;
+    cout << "la nationalite du personne est :  " << nationalite << endl;
+    cout << "la date de naissance du personne est :  " << date_naissance << endl;
+}
+void personnes::modifier_personne()
+{
+    int choix;
+    cout << "si voulez vous modidfier le champ de cin de personne taper 1 " << endl;
+    cout << "si voulez vous modidfier le champ de nom de peesonne taper 2 " << endl;
+    cout << "si voulez modidfier le champ de prenom de personnetaper 3 " << endl;
+    cout << "si voulez modidfier le champ de nationalite de personne taper 4 " << endl;
+    cout << "si voulez modidfier le champ de date naissance de personne taper 5 " << endl;
+    cout << "donner le choix" << endl;
+    cin >> choix;
+    if (choix == 1)
+    {
+        int modification;
+        cout << "donner la modification " << endl;
+        cin >> modification;
+        cin_p = modification;
+    }
+    else if (choix == 2)
+    {
+        string modification;
+        cout << "donner la modification " << endl;
+        cin >> modification;
+        nom = modification;
+    }
+    else if (choix == 3)
+    {
+        string modification;
+        cout << "donner la modification " << endl;
+        cin >> modification;
+        prenom = modification;
+    }
+    else if (choix == 4)
+    {
+        string modification;
+        cout << "donner la modification " << endl;
+        cin >> modification;
+        nationalite = modification;
+    }
+    else if (choix == 5)
+    {
+        string modification;
+        cout << "donner la modification " << endl;
+        cin >> modification;
+        date_naissance = modification;
+    }
 }
