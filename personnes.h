e8a311eb9604c414b74ff7fe65bc92b61f073515
Mekni Jihed <160278155+Mekni12416@users.noreply.github.com>
2024-02-24 14:50:17 +0100

    add personnes.h

---
+++ personnes.h
@@ -0,0 +1,26 @@
+#ifndef personnes_h
+#define personnes_h
+using namespace std;
+#include <string>
+//#include"date.h"
+//#include<vector>
+class personnes
+{
+protected:
+    int cin_p;
+    string nom;
+    string prenom;
+    string nationalite;
+    string date_naissance;
+
+
+public:
+   personnes();
+   personnes(int,string,string,string,string);
+   void set_personne();
+   void get_personne();
+  
+
+};
+
+#endif
