169b5a3f3b52ea915bd4daef88b6da7c196686b2
Mekni Jihed <160278155+Mekni12416@users.noreply.github.com>
2024-02-24 14:39:33 +0100

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include "personnes.h"
+#include <string>
+
+int main()
+{
+    personnes p;
+    cout<<"bonjour";
+    p.set_personne();
+
+    return 0;
+}
